1153e3a50adfceabd8b136813040cb448a8c2725
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-11 20:30:53 +0000

    Fixing task errors

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
 int binary_tree_b(const binary_tree_t *tree);
+size_t binary_tree_h(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Measures the balance of a binary tree
@@ -16,7 +17,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	balance = binary_tree_b(tree);
 
-	return (balance - 1);
+	return (balance);
 }
 
 /**
@@ -27,13 +28,32 @@ int binary_tree_balance(const binary_tree_t *tree)
 int binary_tree_b(const binary_tree_t *tree)
 {
 
-	int left_b, right_b;
+	size_t left_b, right_b;
+
+	left_b = binary_tree_h(tree->left);
+	right_b = binary_tree_h(tree->right);
+
+	return (left_b - right_b);
+}
+
+/**
+ * binary_tree_h - Measures the height of a binary tree node
+ * @tree: Tree node to measure
+ * Return: The height of the binary tree node
+ */
+size_t binary_tree_h(const binary_tree_t *tree)
+{
+
+	size_t left_h, right_h;
 
 	if (!tree)
 		return (0);
 
-	left_b = binary_tree_b(tree->left);
-	right_b = binary_tree_b(tree->right);
+	left_h = binary_tree_h(tree->left);
+	right_h = binary_tree_h(tree->right);
 
-	return (left_b - right_b + 1);
+	if (left_h > right_h)
+		return (left_h + 1);
+	else
+		return (right_h + 1);
 }
